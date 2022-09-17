61021c0a1ac2d941b2dd96525930f865dbde8657
apurba2002 <debnathapurba582@gmail.com>
2022-09-17 17:54:44 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// for last index need length and substring 
+// for first need only substring 
+Console.WriteLine("Enter your credit card number :");
+string cname = Console.ReadLine();
+int len = cname.Length;
+if ( len <= 4 )
+{
+    Console.WriteLine(cname);
+    Console.ReadKey();
+}
+else
+{
+    string first = "**************";
+    string last = cname.Substring(cname.Length -4);
+    Console.WriteLine(first+last);
+    Console.ReadKey();
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# credit-card-number-hide
+ 
